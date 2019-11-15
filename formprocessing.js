b42ac76af9f886e61026658118369faeec9c6063
dcuebas <57089619+dcuebas@users.noreply.github.com>
2019-11-15 16:24:50 +0900

    Update formprocessing.js

--- formprocessing.js
+++ formprocessing.js
@@ -20,7 +20,7 @@ $(document).ready(function(){
         //alert(formData);
         //alert(formData.name + formData.reserveStart + formData.reserveEnd);
         
-        myServer.open("POST", "https://cors-anywhere.herokuapp.com/uranium.snu.ac.kr:7780/reserve", true);
+        myServer.open("GET", "https://cors-anywhere.herokuapp.com/uranium.snu.ac.kr:7780/reserve", true);
         
         //do i have to parse the data? in what form should i parse it?
         myServer.send(formData);
