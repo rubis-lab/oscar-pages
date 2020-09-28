44299937bb444091cd4d4087fd095a216ea97128
jinchoi96 <babjinny@naver.com>
2020-09-28 20:07:28 +0900

    modify busy function

--- _docker-node/index.js
+++ _docker-node/index.js
@@ -552,10 +552,10 @@ var server = http.createServer(function(request,response){
               }
             }
             response.writeHead(200, {'Content-Type':'text/html'});
-            response.end('System is reserved by '+user.name+'_'+startTime+'_'+endTime+'_'+pwd+'_'+selImage);
+            response.end('System is reserved by '+user.name+'_'+startTime+'_'+endTime+'_'+pwd+'_'+selImage+'\n');
           }else{
             response.writeHead(200, {'Content-Type':'text/html'});
-            response.end('System is not busy.');
+            response.end('System is not busy.\n');
           }
         }
       });
