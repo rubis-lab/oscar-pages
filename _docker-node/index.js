a610b8fbe55a04104c7419bfb9b355ebf216c07c
dcuebas <dianecuebas@gmail.com>
2020-10-07 15:08:53 +0900

    Update index.js

--- _docker-node/index.js
+++ _docker-node/index.js
@@ -568,7 +568,7 @@ var server = http.createServer(function(request,response){
       });
   }else if(resource == '/soon'){
     //Same as busy function but is true five minutes before
-    User.findOne({"reservations.reserveStart":{ $gte : new Date(Date.now()).toISOString()}, "reservations.reserveStart": { $lte: new Date(Date.now() + 300000).toISOString()}}
+    User.findOne({$and: [{"reservations.reserveStart":{ $gte : new Date(Date.now()).toISOString()}}, {"reservations.reserveStart": { $lte: new Date(Date.now() + 300000).toISOString()}}]}
                  , function(error,data){
         console.log('--- Reservation list ---');
         console.log(new Date(Date.now()+ 300000).toISOString());
