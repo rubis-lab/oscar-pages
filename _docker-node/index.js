874ef67dad2fb3ab2d7e73b56231d74f5b65a301
dcuebas <dianecuebas@gmail.com>
2020-10-12 13:14:16 +0900

    Update index.js

--- _docker-node/index.js
+++ _docker-node/index.js
@@ -309,8 +309,9 @@ var server = http.createServer(function(request,response){
 
           }else{
             // ** This is not working as expected. If a reservation matches the first condition, it returns duplicate
-            User.findOne({$and :[{"reservations.reserveEnd" : {$gte: parsedQuery.reserveEnd}}, {"reservations.reserveStart" : {$gte: parsedQuery.reserveStart}},
-              {"reservations.reserveStart" : {$lte: parsedQuery.reserveEnd}}]}, function(error,reserved){
+            // User.findOne({$and :[{"reservations.reserveEnd" : {$gte: parsedQuery.reserveEnd}}, {"reservations.reserveStart" : {$gte: parsedQuery.reserveStart}},
+            //   {"reservations.reserveStart" : {$lte: parsedQuery.reserveEnd}}]}, function(error,reserved){
+              User.findOne({"reservations.reserveStart" : {$lte: parsedQuery.reserveStart}}, function(error,reserved){
                 if(error){
                   console.log(error);
                 }else{
