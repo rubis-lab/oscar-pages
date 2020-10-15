41a0c4d66b310e248f43f3c79f6e7fe6e2511499
dcuebas <57089619+dcuebas@users.noreply.github.com>
2020-10-16 04:24:07 +0900

    Update index.js

--- _docker-node/index.js
+++ _docker-node/index.js
@@ -669,7 +669,7 @@ var server = http.createServer(function(request,response){
     request.on('end', function () {
       var parsedQuery = querystring.parse(postdata);
       var index = parsedQuery.index;
-      var query = "{ $set:{notifications." + index + ": 'bleghh' }"
+      var query = "{ $set:{notifications." + index + ".body : 'bleghh' }"
       //, $pull: {notifications : null}}";
       User.findOneAndUpdate({name:parsedQuery.name}, query ,function(error,data){
           //reservations is an array and it must be access through the elements of the area -- reservations[5] == status field
