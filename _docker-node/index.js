5a1670aa5cca8e849d92a9791484461c817e0f35
dcuebas <dianecuebas@gmail.com>
2020-10-30 20:30:04 +0900

    Update index.js
    adding announcement function

--- _docker-node/index.js
+++ _docker-node/index.js
@@ -742,6 +742,27 @@ var server = http.createServer(function(request,response){
           }
         });
     });
+  }else if(resource == '/addannouncement'){
+    //Clears the selected notification
+    var postdata = '';
+    request.on('data', function (data) {
+      postdata = postdata + data;
+    });
+    request.on('end', function () {
+      var parsedQuery = querystring.parse(postdata);
+      var bodyMessage = parsedQuery.text;
+
+      User.updateMany({}, {$push: {notifications:{notif_type: "announce", body: bodyMessage}}},function(error,data){
+          if(error){
+            console.log(error);
+          }else{
+              console.log('--- New Announcement Posted ---');
+              console.log(bodyMessage);
+              response.writeHead(200, {'Content-Type':'text/html'});
+              response.end('Announcement successfully posted.');
+          }
+        });
+    });
   }else if(resource == '/removeAll'){
     //Clear db
       User.deleteMany({});
